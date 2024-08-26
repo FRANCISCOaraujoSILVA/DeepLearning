fad18446241302b265463e2144bb715207cd9644
DELL <franciscoaraujo2016f2@outlook.com>
2024-08-25 21:42:46 -0300

    Contribuições Classificação Multiclasse Base Iris

--- Classificacao_Multiclasse_Base_Iris/2 - iris_cruzada.py
+++ Classificacao_Multiclasse_Base_Iris/2 - iris_cruzada.py
@@ -1,13 +1,15 @@
 import pandas as pd
+from scikeras.wrappers import KerasClassifier
+from sklearn.preprocessing import LabelEncoder
+from keras._tf_keras.keras.utils import to_categorical
+from keras._tf_keras.keras import backend as k
 from keras._tf_keras.keras.models import Sequential
 from keras._tf_keras.keras.layers import Dense
-from sklearn.preprocessing import LabelEncoder
-from scikeras.wrappers import KerasClassifier
 from sklearn.model_selection import cross_val_score
-from keras._tf_keras.keras.utils import to_categorical
 
 base = pd.read_csv("\\Users\\DELL\\PycharmProjects\\DeepLearning_DNN\\Classificacao_Multiclasse_Base_Iris\\iris.csv")
 previsores = base.iloc[:, 0:4].values
+
 classe = base.iloc[:, 4].values
 labelencoder = LabelEncoder()
 classe = labelencoder.fit_transform(classe)
@@ -15,6 +17,7 @@ classe_dummy = to_categorical(classe)
 
 
 def criar_rede():
+    k.clear_session()
     classificador = Sequential()
     classificador.add(Dense(units=4, activation='relu', input_dim=4))
     classificador.add(Dense(units=4, activation='relu'))
@@ -23,12 +26,13 @@ def criar_rede():
     return classificador
 
 
-classificador = KerasClassifier(build_fn=criar_rede, epochs=1000, batch_size=10)
+classificador = KerasClassifier(model=criar_rede, epochs=1000, batch_size=10)
 resultados = cross_val_score(estimator=classificador,
                              X=previsores, y=classe_dummy,
                              cv=10, scoring='accuracy')
 
-# Não colocar a classe_dummy em y?
-
+print(' ')
 media = resultados.mean()
+print(f'Acurácia: {media}')
 desvio = resultados.std()
+print(f'Desvio padrão: {desvio}')
